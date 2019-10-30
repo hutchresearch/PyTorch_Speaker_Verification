eb7a427c988a543ac2611c39e18ac8d489d7448b
Eric Slyman <slymane@wwu.edu>
2019-10-30 11:29:18 -0700

    Add dataset linking scripts for CAAML and TIMIT

---
+++ link_caaml.sh
@@ -0,0 +1,17 @@
+BASE_PATH=/home/hutch_research/data/2019_ml_teaching
+INSTRUCTORS=(
+    100
+    198
+    203
+    226
+    388
+    504
+    660
+    784
+    925
+)
+
+mkdir ./CAAML
+for INST in "${INSTRUCTORS[@]}"; do
+    ln -s $BASE_PATH/$INST ./CAAML/$INST
+done
---
+++ link_timit.sh
@@ -0,0 +1,4 @@
+mkdir ./TIMIT
+ln -s /home/hutch_research/data/timit/TIMIT/TRAIN ./TIMIT/TRAIN
+ln -s /home/hutch_research/data/timit/TIMIT/DEV   ./TIMIT/DEV
+ln -s /home/hutch_research/data/timit/TIMIT/TEST  ./TIMIT/TEST
